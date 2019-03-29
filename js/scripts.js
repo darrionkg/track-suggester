a89293a041366faee7ca557d96733678343e5a96
Darrion <darrionkg@gmail.com>
2019-03-29 11:34:09 -0700

    Add branching for questions 4, 5, and results

--- js/scripts.js
+++ js/scripts.js
@@ -45,12 +45,84 @@ $(document).ready(function() {
       java += 1;
       cSharp += 1;
     }
+    //Question 4
+    if(answer4 === "web") {
+      ruby += 1;
+      php += 1;
+      css += 1;
+      cSharp += 1;
+    } else {
+      java += 1;
+    }
 
-    console.log(answer1);
-    console.log(answer2);
-    console.log(answer3);
-    console.log(answer4);
-    console.log(answer5);
+    //Question 5
+    if(answer5 === "ruby") {
+      ruby += 2;
+    } else if(answer5 === "php") {
+      php += 2;
+    } else if(answer5 === "java") {
+      java += 2;
+    } else if(answer5 === "css") {
+      css += 2;
+    } else if(answer5 === "cSharp") {
+      cSharp += 2;
+    }
+
+    //Shows Results. If 2 values are equal, shows both.
+    if(ruby >= php && ruby >= java && ruby >= css && ruby >= cSharp) {
+      (".rubyRails").show();
+      if(ruby === php) {
+        (".phpDrupal").show();
+      } else if(ruby === java) {
+        (".javaAndroid").show();
+      } else if(ruby === css) {
+        (".cssDesign").show();
+      } else if(ruby === cSharp)
+        (".cSharpNet").show();
+    } else if(php >= ruby && php >= java && php >= css && php >= cSharp) {
+      (".phpDrupal").show();
+      if(php === ruby) {
+        (".rubyRails").show();
+      } else if(php === java) {
+        (".javaAndroid").show();
+      } else if(php === css) {
+        (".cssDesign").show();
+      } else if(php === cSharp)
+        (".cSharpNet").show();
+    } else if(java >= ruby && java >= php && java >= css && java >= cSharp) {
+      (".javaAndroid").show();
+      if(java === ruby) {
+        (".rubyRails").show();
+      } else if(java === php) {
+        (".phpDrupal").show();
+      } else if(java === css) {
+        (".cssDesign").show();
+      } else if(java === cSharp)
+        (".cSharpNet").show();
+    } else if(css >= ruby && css >= php && css >= java && css >= cSharp) {
+      (".cssDesign").show();
+      if(css === ruby) {
+        (".rubyRails").show();
+      } else if(css === php) {
+        (".phpDrupal").show();
+      } else if(css === java) {
+        (".javaAndroid").show();
+      } else if(css === cSharp)
+        (".cSharpNet").show();
+    } else if(cSharp >= ruby && cSharp >= php && cSharp >= java && cSharp >= cSharp) {
+      (".cSharpNet").show();
+      if(cSharp === ruby) {
+        (".rubyRails").show();
+      } else if(cSharp === php) {
+        (".phpDrupal").show();
+      } else if(cSharp === java) {
+        (".javaAndroid").show();
+      } else if(cSharp === css)
+        (".cssDesign").show();
+      } else {
+        console.log("Unknown Error.")
+      }
+
     event.preventDefault();
 
   });
