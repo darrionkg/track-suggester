4dcdf98778b26d24a8710c6358e436463feb096a
Darrion <darrionkg@gmail.com>
2019-03-29 10:30:01 -0700

    Add variables to store radio and select box inputs in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
-  $("#the-form").submit(function() {
-
+  $("#surveyForm").submit(function(event) {
+    var answer1 = $("input:radio[name=environment]:checked").val();
+    var answer2 = $("input:radio[name=frontBackEnd]:checked").val();
+    var answer3 = $("input:radio[name=problemSolving]:checked").val();
+    var answer4 = $("webMobile").val();
+    var answer5 = $("language").val();
+    console.log(answer1);
+    event.preventDefault();
+    //if(".question1" === "")
   });
 });
