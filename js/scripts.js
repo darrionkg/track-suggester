cafc5deaa5da7df5b0c94dc124dd0fd9b0a93b18
Darrion <darrionkg@gmail.com>
2019-03-29 10:56:54 -0700

    Add language variables and branch for answer1 to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,37 @@ $(document).ready(function() {
     var answer1 = $("input:radio[name=environment]:checked").val();
     var answer2 = $("input:radio[name=frontBackEnd]:checked").val();
     var answer3 = $("input:radio[name=problemSolving]:checked").val();
-    var answer4 = $("webMobile").val();
-    var answer5 = $("language").val();
+    var answer4 = $("#webMobile").val();
+    var answer5 = $("#language").val();
+    // language points
+    var ruby = 0
+    var php = 0;
+    var java = 0;
+    var css = 0;
+    var cSharp = 0;
+
+    if(answer1 === "startup") {
+      ruby += 1;
+      php += 1;
+    } else if(answer1 === "small") {
+      ruby += 1;
+      css += 1;
+      cSharp += 1;
+      java += 1;
+    } else if(answer1 === "large") {
+      cSharp += 1;
+      java += 1;
+    } else {
+      php += 1;
+      css += 1;
+    }
+    
     console.log(answer1);
+    console.log(answer2);
+    console.log(answer3);
+    console.log(answer4);
+    console.log(answer5);
     event.preventDefault();
-    //if(".question1" === "")
+
   });
 });
